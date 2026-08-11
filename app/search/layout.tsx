57a8cd14552b6a26032a0e18140b9087249b0c74
jakemsr <jakemsr@yahoo.com>
2026-08-11 15:36:29 -0700

    feat: Add a header to get back home

---
+++ app/search/layout.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function SearchLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="">
+      <div className="flex justify-between items-center text-sm sm:text-base bg-gray-100 p-4">
+        <Link href="/" className="text-blue-700 hover:underline">Home</Link>
+        <span className="text-center">OpenBSD Device Support Database</span>
+        <Link href="/login" className="text-blue-700 cursor-not-allowed pointer-events-none">Login</Link>
+      </div>
+
+      <section className="w-full">
+        {children}
+      </section>
+    </div>
+  );
+}
