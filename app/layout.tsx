3cd48be1e65c2a28576402256ce5d77f140a5e06
jakemsr <jakemsr@yahoo.com>
2026-09-10 20:33:28 -0700

    fix: Use local copy of font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local';
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const customFont = localFont({
+  src: '../public/fonts/Geist-VariableFont_wght.ttf',
+  variable: '--font-custom',
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -27,7 +24,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${customFont.variable} h-full antialiased`}
     >
       <body className="min-h-svh flex flex-col">
         <Header />
